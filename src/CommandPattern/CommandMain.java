5237c76f5c5f12f1d22e645ea08821cfe540ace6
Thaitophuoc <tophuocthai@gmail.com>
2020-07-05 10:40:52 +0700

    Sửa lại bài 3

--- src/CommandPattern/CommandMain.java
+++ src/CommandPattern/CommandMain.java
@@ -11,22 +11,18 @@ package CommandPattern;
  */
 public class CommandMain {
     public static void main(String[] args) {
-      giohang gh = new giohang();
-      Store store = new Store();
-      
-      gh.duyethang(new Chitietgiohang("USB", 5, 10000));
-      gh.duyethang(new Chitietgiohang("CPU", 6, 20000));
-      gh.duyethang(new Chitietgiohang("RAM", 2, 30000));
-      
-      store.nhangiohang(gh);
-      gh.inhoadon(store);
-      
-      gh.huyduyethang();
-      
-      store.nhangiohang(gh);
-      
-      System.out.println("\n Sau khi hủy: ");
-      gh.inhoadon(store);
-      
+        Store store = new Store();
+        giohang gh = new giohang();
+        Command cmd1 = new Command(new Chitietgiohang("CPU", 1, 100),store);
+        Command cmd2 = new Command(new Chitietgiohang("RAM", 3, 50),store);
+        Command cmd3 = new Command(new Chitietgiohang("USB", 2, 40),store);
+        
+        gh.duyet(cmd1);
+        gh.duyet(cmd2);
+        gh.duyet(cmd3);
+        gh.xacnhanmua();
+        
+        gh.huyduyet();
+        gh.xacnhanmua();
     }
 }
