25e88212ce3aba5d4f20dc0659d6f8ded451457e
Thaitophuoc <tophuocthai@gmail.com>
2020-07-05 19:35:57 +0700

    Sửa lại bài 3

---
+++ src/Facadepattern/facade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facadepattern;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tophu
+ */
+public interface facade {
+    public void hienthids();
+    public void inhoadon();
+    public void hienthi_va_demsoluong();
+}
